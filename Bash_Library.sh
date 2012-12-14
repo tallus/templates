3f4c14ef22539800acf6ef9deefc92648c3be514
paulm <paulm@freegeek.org>
2012-12-14 12:37:48 -0800

    initial commit

---
+++ Bash_Library.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+###############
+# Conventions #
+###############
+
+# All scripts will start ts_ 
+# All libraries will have a second part that acts as a prefix
+# finally there will be an actual human readable name
+# eg ts_rt_library-name.sh
+
+# All functions will start with the prefix followed by a dot. e.g. rt.fuction_name
+
+# all libraries will:
+#       include a prefix.describe function 
+#       have a #!/bin/bash line at the top
+#       call this function at the end of the script 
+#       so you can call the script directly to get a description of what it does#       ditto for prefix.list_functions
+
+
+# Don't forget to replace template with actual prefix
+
+#Describe what this library does
+template.describe(){ cat <<EOF
+
+This file is intended as a bash library. 
+Calling it directly merely  prints this description.
+
+EOF
+}
+
+#List functions library contains
+template.list_functions(){ cat <<EOF
+
+EOF
+}
+
+# END OF FILE, FUNCTIONS GO ABOVE THIS LINE
+
+# Don't forget to replace template with actual prefix
+
+# Describe what this library does, and what it contains
+template.describe
+template.list_functions
+
---
+++ Bash_Script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# this file has the following standard layout
+# CONFIGURATION
+# help function
+# INCLUDES
+#FUNCTIONS
+# process option arguments
+# MAIN
+
+# CONFIGURATION
+
+help(){
+cat <<EOF 
+
+usage: $0 [OPTION]...
+This is the summary of what this script does.
+	
+-h			prints this message
+
+This is a bash script. This part is a longer explanation of what it does. 
+EOF
+
+# if $1 exists and is a number 0..255 return that otherwise return 0
+if [[ -n $1 && $(echo {0..255}) =~ $1 ]]; then
+        exit $1
+else
+	exit 0
+fi
+}
+# INCLUDES
+#FUNCTIONS
+
+# process option arguments
+while getopts "h" option; do		# w: place variable following w in $OPTARG
+	case "$option" in
+		h) help;;
+		[?])  echo "bad option supplied" ; 
+			help;;	
+	esac
+done
+
+#MAIN

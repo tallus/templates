edb550dc767a4307a6b7d919a8f2697c2f11f023
paulm <paulm@freegeek.org>
2013-08-16 12:56:16 -0700

    moved unit tests to own file

---
+++ test_template.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import unittest
+import * from  [INSERT MODULE HERE] 
+class MyTests(unittest.TestCase):
+        # pylint: disable=R0904
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_example(self):
+        pass
+
+if __name__ == "__main__":
+        unittest.main()
+
